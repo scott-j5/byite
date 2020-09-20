9a7bf93b8feeed25dd1e1b12ef8b5e60f54328c4
Scott James <scotty.james95@icloud.com>
2020-09-19 17:47:45 -0700

    minor changes to settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DJANGO_DEBUG'))
 
-ALLOWED_HOSTS = ['byitegroup.com', '127.0.0.1']
+ALLOWED_HOSTS = ['byitegroup.com', '3.129.182.170', '127.0.0.1']
 
 
 # Application definition
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR , 'db.sqlite3'),
     }
 }
 
