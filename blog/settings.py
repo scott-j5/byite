6aa5608706300ea5d62404dd590828ddb8a3ae59
Scott James <scotty.james95@icloud.com>
2020-10-02 17:15:48 -0700

    Minor changes for deployment

--- blog/settings.py
+++ blog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DJANGO_DEBUG'))
 
-ALLOWED_HOSTS = ['byitegroup.com', '3.129.182.170', '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS')
 
 
 # Application definition
